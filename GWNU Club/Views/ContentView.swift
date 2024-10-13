02d2f38ff7d6d34eea87513f618db477e0e8fc11
Semin <tpals1127@naver.com>
2024-10-14 02:16:10 +0900

    feat: 앱 아이콘 및 스플래시 화면 (#5)

---
+++ GWNU Club/Views/ContentView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  GWNU Club
+//
+//  Created by 박세민 on 10/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var isLaunch: Bool = true
+    
+    var body: some View {
+        
+        if isLaunch {
+            
+            SplashView()
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.2) {
+                        withAnimation(.linear) {
+                            self.isLaunch = false
+                        }
+                    }
+                }
+            
+        } else {
+            
+            VStack {
+                
+                TabView {
+                    Tab("홈", systemImage: "house") {
+                        VStack {
+                            Text("Home")
+                        }
+                    }
+                    
+                    Tab("동아리", systemImage: "person.3.fill") {
+                        Text("Club")
+                    }
+                    
+                    Tab("이벤트", systemImage: "party.popper") {
+                        Text("Event")
+                    }
+                    
+                    Tab("설정", systemImage: "gearshape") {
+                        Text("Set")
+                    }
+                    
+                }
+                .tint(.primaryColor)
+                .onAppear {
+                    UITabBar.appearance().backgroundColor = .white
+                    UITabBar.appearance().unselectedItemTintColor = .lightGray
+                }
+                
+            }
+            
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
